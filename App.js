98624d176990f0a5816cd713d9068b86558ba474
ngovanan <ngovanan182000@gmail.com>
2020-10-11 21:54:44 +0700

    ..

--- App.js
+++ App.js
@@ -1,21 +1,169 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Linking,
+} from "react-native";
+import moment from "moment";
+import { Icon } from "react-native-elements";
+import { Card, Button } from "react-native-elements";
+
+const filterForUniqueArticles = (arr) => {
+  const cleaned = [];
+  arr.forEach((itm) => {
+    let unique = true;
+    cleaned.forEach((itm2) => {
+      const isEqual = JSON.stringify(itm) === JSON.stringify(itm2);
+      if (isEqual) unique = false;
+    });
+    if (unique) cleaned.push(itm);
+  });
+  return cleaned;
+};
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasErrored, setHasApiError] = useState(false);
+  const [lastPageReached, setLastPageReached] = useState(false);
+
+  const onPress = (url) => {
+    Linking.canOpenURL(url).then((supported) => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    });
+  };
+
+  const getNews = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=6eec2f7fe6cd4c40a3fef8f33f5778fe&page=${pageNumber}`
+      );
+      const jsonData = await response.json();
+      const newArticleList = filterForUniqueArticles(
+        articles.concat(jsonData.articles)
+      );
+      setArticles(newArticleList);
+      setPageNumber(pageNumber + 1);
+      const hasMoreArticles = jsonData.articles.length > 0;
+      if (hasMoreArticles) {
+        const newArticleList = filterForUniqueArticles(
+          articles.concat(jsonData.articles)
+        );
+        setArticles(newArticleList);
+        setPageNumber(pageNumber + 1);
+      } else {
+        setLastPageReached(true);
+      }
+    } catch (error) {
+      setHasApiError(true);
+    }
+    setLoading(false);
+  };
+  const renderArticleItem = ({ item }) => {
+    return (
+      <Card title={item.title} image={{ uri: item.urlToImage }}>
+        <View style={styles.row}>
+          <Text style={styles.label}>Source</Text>
+          <Text style={styles.info}>{item.source.name}</Text>
+        </View>
+        <Text style={{ marginBottom: 10 }}>{item.content}</Text>
+        <View style={styles.row}>
+          <Text style={styles.label}>Published</Text>
+          <Text style={styles.info}>
+            {moment(item.publishedAt).format("LLL")}
+          </Text>
+        </View>
+        <Button
+          icon={<Icon />}
+          title="Read more"
+          backgroundColor="#03A9F4"
+          onPress={() => onPress(item.url)}
+        />
+      </Card>
+    );
+  };
+
+  useEffect(() => {
+    getNews();
+  }, [articles]);
+
+  if (hasErrored) {
+    return (
+      <View style={styles.container}>
+        <Text>Error =(</Text>
+      </View>
+    );
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <Text style={styles.label}>Articles Count:</Text>
+        <Text style={styles.info}>{articles.length}</Text>
+      </View>
+      <FlatList
+        data={articles}
+        renderItem={renderArticleItem}
+        keyExtractor={(item) => item.title}
+        onEndReached={getNews}
+        onEndReachedThreshold={1}
+        ListFooterComponent={
+          lastPageReached ? <Text>No more articles</Text> : <ActivityIndicator
+            size="large"
+            color="#00ff00"
+          />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  containerFlex: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  header: {
+    height: 30,
+    width: "100%",
+    backgroundColor: "pink",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  label: {
+    fontSize: 16,
+    color: "black",
+    marginRight: 10,
+    fontWeight: "bold",
+  },
+  info: {
+    fontSize: 16,
+    color: "grey",
   },
 });
